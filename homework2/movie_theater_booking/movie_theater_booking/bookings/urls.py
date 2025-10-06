c8fbb475e60f3e2c6ffa47d390f586592cfc1a09
Mckenna <mdedric2@uccs.edu>
2025-10-06 21:23:54 +0000

    Updates to urls.py

--- homework2/movie_theater_booking/movie_theater_booking/bookings/urls.py
+++ homework2/movie_theater_booking/movie_theater_booking/bookings/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('movies/', movie_list, name='movie_list'),
     path('movies/<int:movie_id>/seats/', seat_booking, name='seat_booking'),
     path('bookings/history/', booking_history, name='booking_history'),
+    path('', include('bookings.urls')),
 ]
 
